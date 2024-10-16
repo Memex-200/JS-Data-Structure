345bd3f087e436a957a72e0f68f008d9b8bef0fe
MeMex <146092850+Memex-200@users.noreply.github.com>
2024-10-16 10:46:03 -0700

    Add files via upload

---
+++ factorial.js
@@ -0,0 +1,15 @@
+// fact(4) ==> 4*3*2*1
+// fact(3) ==> 3*2*1
+
+function factorial(n) {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result = result * i;
+  }
+
+  return result;
+}
+
+console.log(factorial(0));
+console.log(factorial(1));
+console.log(factorial(5));
