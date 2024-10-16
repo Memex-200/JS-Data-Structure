e06d2ae60899f43418c97b4989943ea00914c3a7
MeMex <146092850+Memex-200@users.noreply.github.com>
2024-10-16 14:56:39 -0700

    Add files via upload

---
+++ bubbleSort.js
@@ -0,0 +1,20 @@
+function bubbleSort(arr) {
+  let swapped;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        //swap
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+}
+
+const arr = [8, -3, 9, 7, 4, 0, 1];
+bubbleSort(arr);
+console.log(arr);
