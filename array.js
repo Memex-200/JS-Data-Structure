4c5519f68bca37a55832453e6af51743d29a3a44
MeMex <146092850+Memex-200@users.noreply.github.com>
2024-10-18 11:26:22 -0700

    Add files via upload

---
+++ array.js
@@ -0,0 +1,11 @@
+// array is a data structure that can hold a collection of values
+// can hold mix of data
+
+const arr = [1, 2, 3, 4, 5];
+arr.push(6);
+arr.unshift(0); // add from the start
+console.log(arr);
+arr.pop();
+console.log(arr);
+arr.shift(); // remove from the end
+console.log(arr);
